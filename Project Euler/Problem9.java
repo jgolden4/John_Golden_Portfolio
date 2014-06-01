9a0571c518ed2cee6ba91ae27b104df1381fdcae
jgolden4 <jgolden4@gmail.com>
2014-06-01 16:13:18 -0400

    Adding the problem

--- Project Euler/Problem9.java
+++ Project Euler/Problem9.java
@@ -1,3 +1,15 @@
+/*
+Problem: Special Pythagorean Triplet
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+*/
 import java.lang.Math;
 import java.lang.Long;
 import java.io.*;
